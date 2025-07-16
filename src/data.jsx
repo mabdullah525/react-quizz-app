2597bc0ff5665ad2d95f51329b68f2ca8fac08f3
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-16 10:36:07 -0700

    Add some quizz data

--- src/data.jsx
+++ src/data.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
-
-const data = () => {
-  return (
-    <div>data</div>
-  )
-}
-
-export default data
+const questions = [
+  {
+    q: "What is JavaScript?",
+    a: "Js is a Programing language",
+    b: "A markup language",
+    c: "None",
+    d: "An operating system",
+    ans: "Js is a Programing language"
+  },
+  {
+    q: "What is React?",
+    a: "A JavaScript library for building user interfaces",
+    b: "A CSS framework",
+    c: "A database",
+    d: "A web server",
+    ans: "a"
+  },
+  {
+    q: "What does JSX stand for?",
+    a: "Java Standard Extension",
+    b: "JavaScript XML",
+    c: "Java Syntax Extension",
+    d: "JavaScript Xtreme",
+    ans: "b"
+  },
+  {
+    q: "Which hook is used to manage state in a functional component?",
+    a: "useEffect",
+    b: "useReducer",
+    c: "useMemo",
+    d: "useState",
+    ans: "d"
+  },
+  {
+    q: "Which keyword is used to define a constant in JavaScript?",
+    a: "var",
+    b: "let",
+    c: "const",
+    d: "define",
+    ans: "c"
+  },
+  {
+    q: "What is the output of 'typeof null' in JavaScript?",
+    a: "object",
+    b: "null",
+    c: "undefined",
+    d: "boolean",
+    ans: "a"
+  },
+  {
+    q: "Which method is used to render React elements to the DOM?",
+    a: "React.render()",
+    b: "ReactDOM.render()",
+    c: "renderComponent()",
+    d: "createElement()",
+    ans: "b"
+  },
+  {
+    q: "What is a component in React?",
+    a: "A database table",
+    b: "A function or class that returns UI elements",
+    c: "A CSS style",
+    d: "A type of server",
+    ans: "b"
+  },
+  {
+    q: "Which operator is used to spread array elements in JavaScript?",
+    a: "*",
+    b: "&",
+    c: "...",
+    d: "=>",
+    ans: "c"
+  },
+  {
+    q: "Which React hook is used for side effects?",
+    a: "useState",
+    b: "useRef",
+    c: "useEffect",
+    d: "useCallback",
+    ans: "c"
+  }
+];
