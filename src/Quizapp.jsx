555de9614a9bb6842dd67d41b229da6b8cdb9a9d
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-19 08:05:07 -0700

    Fix quiz question display and options rendering in Quizapp component

--- src/Quizapp.jsx
+++ src/Quizapp.jsx
@@ -9,9 +9,31 @@ const Quizapp = () => {
         <div className='container'>
             <div className='quiz-container'>
                 <div>
-                    <h1>{data.q}</h1>
+                    <h1>{data[0].q}</h1>
+                </div>
+                <div className='options'>
+                    <input type="radio" />
+                    <p>{data[0].a}</p>
+
+                </div>
+                <div className='options'>
+                    <input type="radio" />
+                    <p>{data[0].b}</p>
+
+                </div>
+                <div className='options'>
+                    <input type="radio" />
+                    <p>{data[0].c}</p>
+                </div>
+                <div className='options'>
+                    <input type="radio" />
+                    <p>{data[0].d}</p>
+
                 </div>
 
+            </div>
+            <div className='btns'>
+                <button className='btn'>Next</button>
             </div>
 
         </div>
