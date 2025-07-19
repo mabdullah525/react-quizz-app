9102cbfa33b3c62fe281c23c3c7db7de1d010bc2
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-19 10:21:53 -0700

    Fix quiz question and options rendering to use dynamic index

--- src/Quizapp.jsx
+++ src/Quizapp.jsx
@@ -8,34 +8,34 @@ const Quizapp = () => {
     const [index, setIndex] = useState();
     console.log(data);
     const next = () => {
-        
+
     }
     return (
         <div className="quiz-bg">
             <div className="quiz-card">
                 <div className="quiz-question">
-                    <h1>Q: {data[0].q}</h1>
+                    <h1>Q: {data[index].q}</h1>
                 </div>
 
                 <div className="quiz-options">
                     <label className="quiz-option">
                         <input type="radio" name="option" />
-                        <span>A : {data[0].a}</span>
+                        <span>A : {data[index].a}</span>
                     </label>
 
                     <label className="quiz-option">
                         <input type="radio" name="option" />
-                        <span>B : {data[0].b}</span>
+                        <span>B : {data[index].b}</span>
                     </label>
 
                     <label className="quiz-option">
                         <input type="radio" name="option" />
-                        <span>C : {data[0].c}</span>
+                        <span>C : {data[index].c}</span>
                     </label>
 
                     <label className="quiz-option">
                         <input type="radio" name="option" />
-                        <span>D : {data[0].d}</span>
+                        <span>D : {data[index].d}</span>
                     </label>
                 </div>
 
