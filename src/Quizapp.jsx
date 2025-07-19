d762782d55c977f8cdb39957fd0e0ce2e171b81b
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-19 07:55:24 -0700

    Refactor Quizapp component to use state and display quiz question

--- src/Quizapp.jsx
+++ src/Quizapp.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Data } from './data'
 
 
 const Quizapp = () => {
+    const [data, setData] = useState(Data);
+    console.log(data);
     return (
-        <div>Quizapamx,ap</div>
+        <div className='container'>
+            <div className='quiz-container'>
+                <div>
+                    <h1>{data.q}</h1>
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
