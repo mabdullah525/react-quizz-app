c09e7c78afb725b3cb89681ebb77240706a61e68
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-19 10:33:44 -0700

    Handle Input

--- src/Quizapp.jsx
+++ src/Quizapp.jsx
@@ -5,36 +5,41 @@ import { Data } from './data'
 
 const Quizapp = () => {
     const [data, setData] = useState(Data);
-    const [index, setIndex] = useState();
+    const [index, setIndex] = useState(0);
     console.log(data);
     const next = () => {
+        if (index >= data.length - 1) {
+            alert("Quiz completed!");
+            return;
+        }
 
+        setIndex(index + 1);
     }
     return (
         <div className="quiz-bg">
             <div className="quiz-card">
                 <div className="quiz-question">
-                    <h1>Q: {data[index].q}</h1>
+                    <h1>Q : {data[index].q}</h1>
                 </div>
 
                 <div className="quiz-options">
                     <label className="quiz-option">
-                        <input type="radio" name="option" />
+                        <input type="radio" name="option" onChange={handleInput} />
                         <span>A : {data[index].a}</span>
                     </label>
 
                     <label className="quiz-option">
-                        <input type="radio" name="option" />
+                        <input type="radio" name="option" onChange={handleInput} />
                         <span>B : {data[index].b}</span>
                     </label>
 
                     <label className="quiz-option">
-                        <input type="radio" name="option" />
+                        <input type="radio" name="option" onChange={handleInput} />
                         <span>C : {data[index].c}</span>
                     </label>
 
                     <label className="quiz-option">
-                        <input type="radio" name="option" />
+                        <input type="radio" name="option" onChange={handleInput} />
                         <span>D : {data[index].d}</span>
                     </label>
                 </div>
