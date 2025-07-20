285065b928ea0deb6f827072ed0c4fb6f46b182f
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-20 06:15:42 -0700

    Add score state management and clearChecked function for quiz options

--- src/Quizapp.jsx
+++ src/Quizapp.jsx
@@ -6,6 +6,7 @@ import { Data } from './data'
 const Quizapp = () => {
     const [data, setData] = useState(Data);
     const [index, setIndex] = useState(0);
+    const [score, setScore] = useState(0);
     console.log(data);
     const next = () => {
         if (index >= data.length - 1) {
@@ -15,50 +16,64 @@ const Quizapp = () => {
 
         setIndex(index + 1);
     }
-    const handleInput = (e) => {
-        let chooseValue = e.target.value;
-        console.log(chooseValue);
+
+    const clearChecked = () => {
+        const checked = document.querySelectorAll('.checkedValue');
+        checked.forEach((curVal) => {
+            curVal.checked = false;
+        });
+    };
+
+
+const handleInput = (e) => {
+    let chooseValue = e.target.value;
+    console.log(chooseValue);
+    if (chooseValue === data[index].ans) {
+        setScore(score + 1);
 
 
     }
-    return (
-        <div className="quiz-bg">
-            <div className="quiz-card">
-                <div className="quiz-question">
-                    <h1>Q : {data[index].q}</h1>
-                </div>
 
-                <div className="quiz-options">
-                    <label className="quiz-option">
-                        <input type="radio" name="option" onChange={handleInput} className='checkedValue' value={data[index].a}/>
-                        <span>A : {data[index].a}</span>
-                    </label>
 
-                    <label className="quiz-option">
-                        <input type="radio" name="option" onChange={handleInput} className='checkedValue' value={data[index].b}/>
-                        <span>B : {data[index].b}</span>
-                    </label>
+}
+return (
+    <div className="quiz-bg">
+        <div className="quiz-card">
+            <div className="quiz-question">
+                <h1>Q : {data[index].q}</h1>
+            </div>
 
-                    <label className="quiz-option">
-                        <input type="radio" name="option" onChange={handleInput} className='checkedValue' value={data[index].c}/>
-                        <span>C : {data[index].c}</span>
-                    </label>
+            <div className="quiz-options">
+                <label className="quiz-option">
+                    <input type="radio" name="option" onChange={handleInput} className='checkedValue' value={data[index].a} />
+                    <span>A : {data[index].a}</span>
+                </label>
 
-                    <label className="quiz-option">
-                        <input type="radio" name="option" onChange={handleInput} className='checkedValue' value={data[index].d}/>
-                        <span>D : {data[index].d}</span>
-                    </label>
-                </div>
+                <label className="quiz-option">
+                    <input type="radio" name="option" onChange={handleInput} className='checkedValue' value={data[index].b} />
+                    <span>B : {data[index].b}</span>
+                </label>
 
-                <div className="quiz-button">
-                    <button onClick={next}>Next</button>
-                </div>
+                <label className="quiz-option">
+                    <input type="radio" name="option" onChange={handleInput} className='checkedValue' value={data[index].c} />
+                    <span>C : {data[index].c}</span>
+                </label>
+
+                <label className="quiz-option">
+                    <input type="radio" name="option" onChange={handleInput} className='checkedValue' value={data[index].d} />
+                    <span>D : {data[index].d}</span>
+                </label>
+            </div>
+
+            <div className="quiz-button">
+                <button onClick={next}>Next</button>
             </div>
         </div>
+    </div>
 
 
 
-    )
+)
 }
 
 export default Quizapp
